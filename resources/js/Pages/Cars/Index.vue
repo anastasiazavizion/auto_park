797090ac51ea46d5d21e0e6792289a756a86be34
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-06-24 11:34:09 +0200

    Changed all cars layout

--- resources/js/Pages/Cars/Index.vue
+++ resources/js/Pages/Cars/Index.vue
@@ -1,21 +1,23 @@
 <script setup>
 import Card from "@/Components/Card.vue";
 import { Link } from '@inertiajs/vue3';
+import Price from "@/Components/Price.vue";
 const props = defineProps({
     cars:Array
 })
 </script>
-
 <template>
     <div :v-key="car.id" v-for="car in cars">
         <Card>
-            <div class="flex justify-between">
-                <div class="font-bold">{{$t('Model')}}</div>
-                <div><Link :href="route('cars.show',car.id)">{{car.model}}</Link></div>
-            </div>
-            <div class="flex justify-between">
-                <div>{{$t('Park')}}</div>
-                <div>{{car.park.address}}</div>
+            <div class="grid grid-cols-3">
+                <div class="flex flex-col">
+                    <div class="font-bold"><Link :href="route('cars.show',car.id)">{{car.model}}</Link></div>
+                    <div><img class="w-64" :src="car.image" :alt="car.model"></div>
+                </div>
+                <div class="flex flex-col col-span-2">
+                    <div><Price>{{car.price}}/{{$t('Hour')}}</Price></div>
+                    <div>{{car.park.address}}</div>
+                </div>
             </div>
         </Card>
     </div>
